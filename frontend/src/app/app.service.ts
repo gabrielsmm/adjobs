5c80c5717c26645b50c1dedf2a1d37d7b2759d2e
Gabriel Mendes <gabrielsmm251@gmail.com>
2022-02-05 13:51:35 -0300

    Frontend criado

---
+++ frontend/src/app/app.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Location } from '@angular/common';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  constructor(private _snack: MatSnackBar, private location: Location) { }
+
+  mensagem(str: string){
+    this._snack.open(`${str}`, 'OK', {
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+      duration: 3000
+    })
+  }
+
+  goBack(){
+    this.location.back();
+  }
+}
