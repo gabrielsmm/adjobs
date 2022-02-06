5003eb1fbfa3f6cc82bfd852e847ec3330899d84
Gabriel Mendes <gabrielsmm251@gmail.com>
2022-02-05 22:06:12 -0300

    Footer implementado

--- frontend/src/app/components/comuns/comuns.module.ts
+++ frontend/src/app/components/comuns/comuns.module.ts
@@ -1,16 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   providers: []
 })
