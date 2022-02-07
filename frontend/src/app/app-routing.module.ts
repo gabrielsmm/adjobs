fe709f044bd105ba903d184691984592f6c3e05f
Gabriel Mendes <gabrielsmm251@gmail.com>
2022-02-06 22:34:57 -0300

    Tela de cadastro melhorada

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
-import { PaginaPrincipalComponent } from './components/views/pagina-principal/pagina-principal.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: 'inicio', loadChildren: () => import('./components/views/pagina-principal/pagina-principal-routing.module').then(m => m.PaginaPrincipalRoutingModule)},
   {path: 'login', loadChildren: () => import('./components/views/login/login-routing.module').then(m => m.LoginRoutingModule)},
-  {path: '', redirectTo: '/inicio', pathMatch: 'full'},
-  {path: '**', component: PaginaPrincipalComponent}
+  {path: '**', loadChildren: () => import('./components/views/pagina-principal/pagina-principal-routing.module').then(m => m.PaginaPrincipalRoutingModule)},
+  {path: '', redirectTo: '/inicio', pathMatch: 'full'}
 ];
 
 @NgModule({
